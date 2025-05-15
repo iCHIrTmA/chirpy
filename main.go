e043f15a05b61e04f1e58c15e75cf35742a30857
iCHirTmA <jonrue.dev@gmail.com>
2025-05-15 21:43:33 +0800

    fileservers

--- main.go
+++ main.go
@@ -6,9 +6,10 @@ import (
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/", http.FileServer(http.Dir(".")))
+
 	server := http.Server{}
 	server.Addr = ":8080"
 	server.Handler = mux
-
 	server.ListenAndServe()
 }
